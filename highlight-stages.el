cb0ac687a50053f56246de70098ea047c8c9574e
zk_phi <zerokay_phi@yahoo.co.jp>
2014-09-14 17:13:59 +0900

    turn into minor-mode / bump version

--- highlight-stages.el
+++ highlight-stages.el
@@ -18,7 +18,7 @@
 
 ;; Author: zk_phi
 ;; URL: http://hins11.yu-yake.com/
-;; Version: 1.0.0
+;; Version: 1.0.1
 
 ;;; Commentary:
 
@@ -33,18 +33,39 @@
 ;;; Change Log:
 
 ;; 1.0.0 first released
+;; 1.0.1 turned into minor-mode
 
 ;;; Code:
 
 (require 'color)
 
-(defconst highlight-stages-version "1.0.0")
+(defconst highlight-stages-version "1.0.1")
 
 ;; + customs
 
 (defgroup highlight-stages nil
-  "highlight staged (quasi-quoted) expressions"
+  "Highlight staged (quasi-quoted) expressions"
   :group 'emacs)
+
+(defcustom highlight-stages-matcher-alist
+  '((lisp-mode
+     highlight-stages-lisp-quote-matcher . highlight-stages-lisp-escape-matcher)
+    (emacs-lisp-mode
+     highlight-stages-lisp-quote-matcher . highlight-stages-lisp-escape-matcher))
+  "List of (MAJOR-MODE . (QUOTE-MATCHER . ESCAPE-MATCHER)).
+
+QUOTE-MATCHER is a function with 1 parameter, LIMIT, which
+searches the next quoted expression. The function must return
+non-nil if succeeded, or nil otherwise. A special value 'real
+also can be returned by the function, when the quote is
+\"real\" (not escapable) quote. This may be useful for lisp-like
+languages. When the function returns non-nil, (match-string 0)
+must be the expression matched.
+
+ESCAPE-MATCHER is a function with 1 parameter, LIMIT, which
+searches the next escaped expression. The function must return
+non-nil if succeeded, or nil otherwise. When the function returns
+non-nil, (match-string 0) must be the expression matched.")
 
 (defcustom highlight-stages-lighten-step -2.5
   "Staged expressions get STEP percent lighter."
@@ -58,23 +79,6 @@
 (defcustom highlight-stages-highlight-priority 1
   "Priority which highlight overlays get."
   :group 'highlight-stages)
-
-;; + vars
-
-(defvar highlight-stages-quote-matcher nil
-  "A function with 1 parameter, LIMIT, which searches the next
-  quoted expression. The function must return non-nil if
-  succeeded, or nil otherwise. A special value 'real also can be
-  returned by the function, when the quote is \"real\" (not
-  escapable) quote. This may be useful for lisp-like
-  languages. When the function returns non-nil, (match-string 0)
-  must be the expression matched.")
-
-(defvar highlight-stages-escape-matcher nil
-  "A function with 1 parameter, LIMIT, which searches the next
-  escaped expression. The function must return non-nil if
-  succeeded, or nil otherwise. When the function returns
-  non-nil, (match-string 0) must be the expression matched.")
 
 ;; + utils
 
@@ -130,44 +134,48 @@
   "Scan and highlight this level."
   (highlight-stages--make-overlay beg end base-level)
   (goto-char beg)
-  (let (quote escape)
-    (while (progn
-             (setq quote (save-excursion
-                           ;; 'real means "real" (non-"quasi") quote
-                           (let ((res (funcall highlight-stages-quote-matcher end)))
-                             (cond ((eq res 'real)
-                                    (cons (match-beginning 0) (cons (match-end 0) t)))
-                                   (res
-                                    (list (match-beginning 0) (match-end 0))))))
-                   escape (save-excursion
-                            (when (funcall highlight-stages-escape-matcher end)
-                              (list (match-beginning 0) (match-end 0)))))
-             (or quote escape))
-      (cond ((or (null escape)
-                 (and quote (< (car quote) (car escape))))
-             (save-excursion
-               (cond ((not (cddr quote))
-                      ;; "quasi"-quote -> a staging operator (increment level)
-                      (highlight-stages--jit-highlighter-1
-                       (car quote) (cadr quote) (1+ base-level)))
-                     ((not (zerop base-level))
-                      ;; "real"-quote inside "quasi"-quote -> an ordinary symbol
-                      (highlight-stages--jit-highlighter-1
-                       (car quote) (cadr quote) base-level))
-                     (t
-                      ;; "real"-quote outside "quasi"-quote
-                      (when highlight-stages-highlight-real-quote
-                        (highlight-stages--make-overlay (car quote) (cadr quote) 1)))))
-             (goto-char (cadr quote)))
-            (t
-             (save-excursion
-               (highlight-stages--jit-highlighter-1
-                (car escape) (cadr escape) (1- base-level)))
-             (goto-char (cadr escape)))))))
+  (let* ((pair (assq major-mode highlight-stages-matcher-alist))
+         (quote-matcher (cadr pair))
+         (escape-matcher (cddr pair))
+         quote escape)
+    (when (and quote-matcher escape-matcher)
+      (while (progn
+               (setq quote (save-excursion
+                             ;; 'real means "real" (non-"quasi") quote
+                             (let ((res (funcall quote-matcher end)))
+                               (cond ((eq res 'real)
+                                      (cons (match-beginning 0) (cons (match-end 0) t)))
+                                     (res
+                                      (list (match-beginning 0) (match-end 0))))))
+                     escape (save-excursion
+                              (when (funcall escape-matcher end)
+                                (list (match-beginning 0) (match-end 0)))))
+               (or quote escape))
+        (cond ((or (null escape)
+                   (and quote (< (car quote) (car escape))))
+               (save-excursion
+                 (cond ((not (cddr quote))
+                        ;; "quasi"-quote -> a staging operator (increment level)
+                        (highlight-stages--jit-highlighter-1
+                         (car quote) (cadr quote) (1+ base-level)))
+                       ((not (zerop base-level))
+                        ;; "real"-quote inside "quasi"-quote -> an ordinary symbol
+                        (highlight-stages--jit-highlighter-1
+                         (car quote) (cadr quote) base-level))
+                       (t
+                        ;; "real"-quote outside "quasi"-quote
+                        (when highlight-stages-highlight-real-quote
+                          (highlight-stages--make-overlay (car quote) (cadr quote) 1)))))
+               (goto-char (cadr quote)))
+              (t
+               (save-excursion
+                 (highlight-stages--jit-highlighter-1
+                  (car escape) (cadr escape) (1- base-level)))
+               (goto-char (cadr escape))))))))
 
 ;; + settings for lisp
 
-(defun highlight-stages--lisp-quote-matcher (&optional limit)
+(defun highlight-stages-lisp-quote-matcher (&optional limit)
   (let ((original-pos (point)) syntax realp res)
     (when (search-forward-regexp "`\\|\\(#?'\\)" limit t)
       (setq realp (match-beginning 1))  ; must be done before (syntax-ppss)
@@ -180,13 +188,13 @@
                (set-match-data (list beg end))
                ;; "real" quote or "quasi" quote
                (if realp 'real t)))
-            ((setq res (highlight-stages--lisp-quote-matcher limit))
+            ((setq res (highlight-stages-lisp-quote-matcher limit))
              res)
             (t                          ; not found
              (goto-char original-pos)
              nil)))))
 
-(defun highlight-stages--lisp-escape-matcher (&optional limit)
+(defun highlight-stages-lisp-escape-matcher (&optional limit)
   (let ((original-pos (point)) syntax res)
     (when (search-forward-regexp ",@?" limit t)
       (setq syntax (syntax-ppss))
@@ -197,22 +205,26 @@
                                (point))))
                (set-match-data (list beg end))
                t))
-            ((setq res (highlight-stages--lisp-escape-matcher limit))
+            ((setq res (highlight-stages-lisp-escape-matcher limit))
              res)
             (t
              (goto-char original-pos)
              nil)))))
 
 ;;;###autoload
-(defun highlight-stages-lisp-initialize ()
-  "Setup highlight-stages for Lisp buffers."
-  (interactive)
-  (jit-lock-mode 1)
-  (set (make-local-variable 'highlight-stages-quote-matcher)
-       'highlight-stages--lisp-quote-matcher)
-  (set (make-local-variable 'highlight-stages-escape-matcher)
-       'highlight-stages--lisp-escape-matcher)
-  (jit-lock-register 'highlight-stages-jit-highlighter))
+(define-minor-mode highlight-stages-mode
+  "Highlight staged (quasi-quoted) expressions"
+  :init-value nil
+  :lighter "Stg"
+  :global nil
+  (if highlight-stages-mode
+      (jit-lock-register 'highlight-stages-jit-highlighter)
+    (jit-lock-unregister 'highlight-stages-jit-highlighter)))
+
+;;;###autoload
+(define-globalized-minor-mode highlight-stages-global-mode
+  highlight-stages-mode
+  (lambda () (highlight-stages-mode 1)))
 
 ;; + provide
 
